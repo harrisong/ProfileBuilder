64f40206f4baea51d3b1c7ce7c5fe9723a9f9018
Harrison N <tplawyer@gmail.com>
2017-11-13 17:54:35 -0800

    get rid of useless library

--- server.js
+++ server.js
@@ -1,60 +1,34 @@
 const express = require('express');
-
 const app = express();
 const fs = require('fs');
 const request = require('request');
-const simpleOauth2 = require('simple-oauth2');
 const config = require('./config.json');
 
-// Set the configuration settings
-const credentials = {
-  client: {
-    id: config.client_id,
-    secret: config.client_secret,
-  },
-  auth: {
-    tokenHost: 'https://github.com',
-    authorizePath: '/login/oauth/authorize',
-  },
-};
-
-// Initialize the OAuth2 Library
-const oauth2 = simpleOauth2.create(credentials);
-
-app.get('/callback', (req) => {
+app.get('/callback', (req, res) => {
   const [state, code] = [req.query.state, req.query.code];
   if (state !== 'dummystate') throw new Error('Wrong state');
   if (!code) throw new Error('Empty code');
 
-  const tokenConfig = {
-    code,
-    redirect_uri: `${config.host}/callback`,
-  };
-
-  oauth2.authorizationCode.getToken(tokenConfig)
-    .then((result) => {
-      const accessToken = oauth2.accessToken.create(result);
-      fs.writeFile(config.access_token_file, accessToken, (err) => {
-        if (err) throw err;
-        console.log('Access Token saved');
-      });
-    })
-    .catch((error) => {
-      console.log('Access Token Error', error.message);
+  const result = request({
+    url: 'https://github.com/login/oauth/access_token',
+    method: 'POST',
+    json: {
+      client_id : config.client_id,
+      client_secret : config.client_secret,
+      code : code
+    }
+  }, (error, response, body) => {
+    const access_token = JSON.stringify(body);
+    fs.writeFile(config.access_token_file, access_token, (err) => {
+      if (err) throw err;
+      res.send("Access Token Saved");
     });
+  });
 });
 
 
 app.get('/login', (req, res) => {
-  // Authorization oauth2 URI
-  const authorizationUri = oauth2.authorizationCode.authorizeURL({
-    redirect_uri: `${config.host}/callback`,
-    scope: 'user public_repo repo read:org',
-    state: 'dummystate',
-  });
-
-  // Redirect example using Express (see http://expressjs.com/api.html#res.redirect)
-  res.redirect(authorizationUri);
+  res.redirect(`https://github.com/login/oauth/authorize?client_id=${config.client_id}&scope=user public_repo repo read:org&state=dummystate`);
 });
 
 app.get('/get_commits', () => {
