e461fc512dc44c9daac70425bce3c4bb646b6377
Harrison N <tplawyer@gmail.com>
2017-11-13 20:53:28 -0800

    fix eslint

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const request = require('request');
+const jsonfile = require('jsonfile');
 const config = require('./config.json');
+
+const app = express();
 
 app.get('/callback', (req, res) => {
   const [state, code] = [req.query.state, req.query.code];
   if (state !== 'dummystate') throw new Error('Wrong state');
   if (!code) throw new Error('Empty code');
 
-  const result = request({
+  request({
     url: 'https://github.com/login/oauth/access_token',
     method: 'POST',
     json: {
-      client_id : config.client_id,
-      client_secret : config.client_secret,
-      code : code
-    }
+      client_id: config.client_id,
+      client_secret: config.client_secret,
+      code,
+    },
   }, (error, response, body) => {
-    const access_token = JSON.stringify(body);
-    fs.writeFile(config.access_token_file, access_token, (err) => {
+    const accessToken = JSON.stringify(body);
+    fs.writeFile(config.access_token_file, accessToken, (err) => {
       if (err) throw err;
-      res.send("Access Token Saved");
+      res.send('Access Token Saved');
     });
   });
 });
@@ -31,14 +33,21 @@ app.get('/login', (req, res) => {
   res.redirect(`https://github.com/login/oauth/authorize?client_id=${config.client_id}&scope=user public_repo repo read:org&state=dummystate`);
 });
 
-app.get('/get_commits', () => {
+app.get('/get_commits', (req, res) => {
+  const accessTokenFile = jsonfile.readFileSync(`./${config.access_token_file}`);
+
   request({
     url: 'https://api.github.com/graphql',
     method: 'POST',
+    header: {
+      'User-Agent': 'ProfileBuilder',
+    },
     json: { query: 'query { viewer { login }}' },
     headers: {
-      Authorization: `bearer ${config.token}`,
+      Authorization: `bearer ${accessTokenFile.access_token}`,
     },
+  }, (error, response, body) => {
+    res.send(body);
   });
 });
 
